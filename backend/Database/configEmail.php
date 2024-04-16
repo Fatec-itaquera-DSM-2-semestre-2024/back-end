924d969a65aeed9733d57fb699c0350d93cf788c
Daniel V. O. Rodrigues <daniel.rodrigues46@fatec.sp.gov.br>
2024-04-15 21:11:11 -0300

    init

--- backend/Database/configEmail.php
+++
@@ -1,6 +0,0 @@
-<?php
-  define('MAIL_HOST', 'mail.seuprovedorsite.com');
-  define('MAIL_USERNAME', 'contato@seuprovedorsite.com');
-  define('MAIL_PASSWORD', 'senhadoemail');
-  define('MAIL_FROM', 'contato@seuprovedorsite.com');
-  define('MAIL_PORT', '587');
