d3e1f90471b48772d052923b173b6af6e0846913
Delvecchi <heitordelveccho@hotmail.com>
2024-05-22 13:25:47 -0300

    Criação Router e Controller, não funcionando, necessita verificação

--- src/Controller/SoftwareController.php
+++ src/Controller/SoftwareController.php
@@ -8,5 +8,37 @@ class SoftwareController {
     public function __construct() {
         $this->db = new Model();
     }
-
+    public function select(){
+        $user = $this->db->select('software');
+        
+        return  $user;
+    }
+    public function selectId($id){
+        $user = $this->db->select('software',['id'=>$id]);
+        
+        return  $user;
+    }
+    public function insert($data){
+        if($this->db->insert('software', $data)){
+            return true;
+        }
+        return false;
+    }
+    
+    public function update($newData,$conditions){
+        if($this->db->update('software', $newData, ['id'=>$conditions])){
+            return true;
+        }
+        return false;
+    }
+    public function delete( $id){
+        $resultado =$this->selectId($id);
+        if(count($resultado)<1){
+            return false;
+        }
+        if($this->db->delete('software', ['id'=>$id])){
+            return true;
+        }
+        return false;        
+    }
 }
--- src/Router/Software.php
+++ src/Router/Software.php
@@ -1,3 +1,36 @@
 <?php
+
 namespace App\Router;
+require "../../vendor/autoload.php";
+
+use App\Controller\SoftwareController;
+
+$software = new SoftwareController();
+
+$body = json_decode(file_get_contents('php://input'), true);
+$id = isset($_GET['id'])?$_GET['id']:'';
+switch($_SERVER["REQUEST_METHOD"]){
+    case "POST";
+        $resultado = $software->insert($body);
+        echo json_encode(['status'=>$resultado]);
+    break;
+    case "GET";
+        if(!isset($_GET['id'])){
+            $resultado = $software->select();
+            echo json_encode(["software"=>$resultado]);
+        }else{
+            $resultado = $software->selectId($id);
+            echo json_encode(["status"=>true,"software"=>$resultado[0]]);
+        }
+       
+    break;
+    case "PUT";
+        $resultado = $software->update($body,intval($_GET['id']));
+        echo json_encode(['status'=>$resultado]);
+    break;
+    case "DELETE";
+        $resultado = $software->delete(intval($_GET['id']));
+        echo json_encode(['status'=>$resultado]);
+    break;  
+}
 
